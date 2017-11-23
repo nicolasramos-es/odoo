3eda4c8f677a774f6032a2f2c1465342aa4ea5f2
Nicolás <contacto@nicolasramos.es>
2017-11-23 14:40:37 +0000

    no message

---
+++ tic_remote_support/__manifest__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##############################################
+#
+# Difusión Visual
+# Copyright (C) Difusión Visual
+# all rights reserved
+# http://difusionvisual.com
+# contacto@difusionvisual.com
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs.
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+###############################################
+{
+    'name': "Tic Remote Support",
+
+    'summary': """
+        This module adds new partner tab to add remote access data
+        to your clients such as TeamViewer, VNC, ...""",
+
+    # 'description': put the module description in README.rst
+
+    'author': 'Difusión Visual',
+    'website': "http://difusionvisual.com",
+    'category': 'Extra Rights',
+    'version': '11.0.4.0.0',
+    'license': 'AGPL-3',
+    'images': ['static/description/Banner.png'],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/tic_remote_support.xml',
+    ],
+
+}
